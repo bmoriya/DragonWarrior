58334ed51213b3b4c524ae49dbaf9c9a062426e7
Brian <brian.moriya@capturedbypenguins.com>
2011-09-17 14:31:08 -0700

    Hacked it so that roof tiles are now showing.

--- src/game.py
+++ src/game.py
@@ -1,9 +1,14 @@
 from os import pardir
 from os.path import join
-from pygame import init, error
+from pygame import init, error, Surface
 from pygame.display import set_mode, set_caption, flip
 from pygame.image import load
+from pygame.sprite import Group
 from pygame.transform import scale
+
+from base_sprite import BaseSprite
+#from tantagel_throne_room import TantagelThroneRoom
+from maps import tantagel_throne_room
 
 class Game(object):
     '''
@@ -93,9 +98,84 @@ class Game(object):
         for x, row in enumerate(self.sprites):
             for y, tile in enumerate(row):
                 self.screen.blit(tile, (x*16*self.SCALE, y*16*self.SCALE))
-
+#                print x, y
+        
+        self.roof_group = Group()
+        '''
+        self.wall_group = Group()
+        self.wood_group = Group()
+        self.brick_group = Group()
+        self.chest_group = Group()
+        self.door_group = Group()
+        self.brick_stairdn_group = Group()
+        self.brick_stairup_group = Group()
+        self.barrier_group = Group()
+        self.weapon_sign_group = Group()
+        self.inn_sign_group = Group()
+        self.castle_group = Group()
+        self.current_map = TantagelThroneRoom()
+        '''
+        self.load_map(tantagel_throne_room)
+        
+        bigmap_width = 24 * self.TILE_SIZE
+        bigmap_height = 23 * self.TILE_SIZE
+        self.bigmap = Surface((bigmap_width, bigmap_height))
+        self.bigmap.fill(self.BLACK)
+        
+        self.roof_group.draw(self.bigmap)
+        '''
+        self.wall_group.draw(self.bigmap)
+        self.wood_group.draw(self.bigmap)
+        self.brick_group.draw(self.bigmap)
+        self.chest_group.draw(self.bigmap)
+        self.door_group.draw(self.bigmap)
+        self.brick_stairdn_group.draw(self.bigmap)
+        '''
+        self.bigmap.convert()
+        
+        self.background = Surface(self.screen.get_size())
+        self.background = self.bigmap.subsurface(0, 0, self.WIN_WIDTH, 
+                                                 self.WIN_HEIGHT)
+        self.background.convert()
+        self.screen.blit(self.background, (0, 0))
+        
         flip()
 
+    def load_map(self, current_map):
+        x_offset = self.TILE_SIZE / 2
+        y_offset = self.TILE_SIZE / 2
+
+        for y in range (len(current_map)):
+            for x in range(len(current_map[y])):
+                center_pt = [(x * self.TILE_SIZE) + x_offset,
+                             (y * self.TILE_SIZE) + y_offset]
+                print x, y
+                
+                if current_map[y][x] == self.ROOF:
+                    roof = BaseSprite(center_pt, self.sprites[self.ROOF][0])
+                    self.roof_group.add(roof)
+                elif current_map[y][x] == self.WALL:
+                    wall = BaseSprite(center_pt, self.sprites[self.WALL][1])
+                    self.wall_group.add(wall)
+                '''
+                elif current_map[y][x] == self.WOOD:
+                    wood = BaseSprite(center_pt, self.sprites[self.WOOD])
+                    self.wood_group.add(wood)
+                elif current_map[y][x] == self.BRICK:
+                    brick = BaseSprite(center_pt, self.sprites[self.BRICK])
+                    self.brick_group.add(brick)
+                elif current_map[y][x] == self.CHEST:
+                    chest = BaseSprite(center_pt, self.sprites[self.CHEST])
+                    self.chest_group.add(chest)
+                elif current_map[y][x] == self.DOOR:
+                    door = BaseSprite(center_pt, self.sprites[self.DOOR])
+                    self.door_group.add(door)
+                elif current_map[y][x] == self.BRICK_STAIRDN:
+                    brick_stairdn = BaseSprite(center_pt, 
+                                               self.sprites[
+                            self.BRICK_STAIRDN])
+                    self.brick_stairdn_group.add(brick_stairdn)
+                '''
 
     def load_spritesheet(self, filename, width, height, colorkey=None):
         '''
@@ -114,8 +194,8 @@ class Game(object):
             print e
             return
         image_width, image_height = image.get_size()
-        width *= self.SCALE
-        height *= self.SCALE
+        width = self.TILE_SIZE
+        height = self.TILE_SIZE
         tile_table = []
         
         for x in range(0, image_width / width):
