50822354a97e153dd153d9e48426102c2b42da50
Brian <brian.moriya@capturedbypenguins.com>
2011-09-16 21:42:34 -0700

    Got some constants moved from config to game and initialized the main window.

--- src/game.py
+++ src/game.py
@@ -1,4 +1,42 @@
+from os import pardir
+from os.path import join
+from pygame import init
+from pygame.display import set_mode, set_caption, flip
+
 class Game(object):
+    '''
+    Glue that holds the game together.
+    '''
+    #Native NES resolution
+    NESRES = (256, 240)
+    
+    #Important directories and file paths
+    DATA_DIR = join(pardir, "data")
+    TILE_SHEET = join(DATA_DIR, "tileset.png")
+    CHAR_SHEET = join(DATA_DIR, "char_tiles.png")
+
+    #Scale for the native resolution, min value is 1.
+    SCALE = 3
+    
+    #Size for the game window.
+    WIN_WIDTH = NESRES[0] * SCALE
+    WIN_HEIGHT = NESRES[1] * SCALE
 
     def __init__(self):
+        '''
+        Initialize the game.
+        '''
+        init()
+        set_mode((self.WIN_WIDTH, self.WIN_HEIGHT))
         
+
+    def main(self):
+        '''
+        Function that runs the game.
+        '''
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.main()
